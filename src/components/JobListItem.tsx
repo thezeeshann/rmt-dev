a7a7037ae5b5b7ab418cc09013eab2289ce98ca5
thezeeshann <info.zeeshann@gmail.com>
2024-04-13 22:09:53 +0530

    refactor: update components

--- src/components/JobListItem.tsx
+++ src/components/JobListItem.tsx
@@ -18,7 +18,7 @@ export default function JobListItem({ jobItem, isActive }: JobItemProps) {
         </div>
 
         <div className="job-item__right">
-          <BookmarkIcon />
+          <BookmarkIcon id={jobItem.id} />
           <time className="job-item__time">{jobItem.daysAgo}d</time>
         </div>
       </a>
