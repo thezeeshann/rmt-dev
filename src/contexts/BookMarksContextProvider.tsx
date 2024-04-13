b68b4a8abb5a7c8877f9e315f3bb6f831a1813ce
thezeeshann <info.zeeshann@gmail.com>
2024-04-13 22:07:04 +0530

    feat: added context api

---
+++ src/contexts/BookMarksContextProvider.tsx
@@ -0,0 +1,49 @@
+import { createContext } from "react";
+import { useLocalStorage } from "../lib/hooks";
+// import { JobItemExpanded } from "../lib/types";
+// import { useJobItems } from "../lib/hooks";
+
+type BookmarksContext = {
+  bookmarkedIds: number[];
+  handleToggleBookmark: (id: number) => void;
+  // bookmarkedJobItems: JobItemExpanded[];
+  // isLoading: boolean;
+};
+
+export const BookmarksContext = createContext<BookmarksContext | null>(null);
+
+function BookmarksContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [bookmarkedIds, setBookmarkedIds] = useLocalStorage<number[]>(
+    "bookmarkedIds",
+    []
+  );
+  // const {  jobItems: bookmarkedJobItems, isLoading } = useJobItems(bookmarkedIds);
+
+  const handleToggleBookmark = (id: number) => {
+    if (bookmarkedIds.includes(id)) {
+      setBookmarkedIds((prev) => prev.filter((item) => item !== id));
+    } else {
+      setBookmarkedIds((prev) => [...prev, id]);
+    }
+  };
+
+  return (
+    <BookmarksContext.Provider
+      value={{
+        bookmarkedIds,
+        handleToggleBookmark,
+        // bookmarkedJobItems,
+        // isLoading,
+      }}
+    >
+      {children}
+    </BookmarksContext.Provider>
+  );
+}
+
+
+export default BookmarksContextProvider
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import BookmarksContextProvider from "./contexts/BookMarksContextProvider.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BookmarksContextProvider>
+        <App />
+      </BookmarksContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
