2c17f9bcc63ea7e7df559ca9755c7b7b72c1df13
thezeeshann <info.zeeshann@gmail.com>
2024-04-18 22:14:16 +0530

    feat: added bookmark components

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,20 +1,27 @@
 import { TriangleDownIcon } from "@radix-ui/react-icons";
 import BookmarksPopover from "./BookmarksPopover";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useOnClickOutside } from "../lib/hooks";
 
 export default function BookmarksButton() {
+
   const [isOpen, setIsOpen] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>(null)
+  const popoverRef = useRef<HTMLDivElement>(null)
+  useOnClickOutside([buttonRef,popoverRef],()=>setIsOpen(false))
+ 
 
   return (
     <section>
       <button
+      ref={buttonRef}
         onClick={() => setIsOpen((prev) => !prev)}
         className="bookmarks-btn"
       >
         Bookmarks <TriangleDownIcon />
       </button>
 
-      {isOpen && <BookmarksPopover />}
+      {isOpen && <BookmarksPopover ref={popoverRef} />}
     </section>
   );
 }
--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,12 +1,17 @@
+import { forwardRef } from "react";
 import { useBookmarksContext } from "../lib/hooks";
 import JobList from "./JobList";
+import { createPortal } from "react-dom";
 
-export default function BookmarksPopover() {
+const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
   const { bookmarkedJobItems, isLoading } = useBookmarksContext();
 
-  return (
-    <div className="bookmarks-popover">
+  return createPortal(
+    <div ref={ref} className="bookmarks-popover">
       <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
-    </div>
+    </div>,
+    document.body
   );
-}
+});
+
+export default BookmarksPopover;
